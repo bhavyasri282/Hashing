e8f916fccf1031176598b6b5739a617a1d21f730
Bhavya Sri <24r21a6767@mlrit.ac.in>
2026-07-21 18:29:58 +0530

    put_function

---
+++ put_function.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class put_function{
+    public static void main(String args[]){
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(1,"bhavya");
+        map.put(2,"navya");
+        map.put(3,"divya");
+        map.put(4,"swetha");
+        System.out.println(map);
+    }
+}
